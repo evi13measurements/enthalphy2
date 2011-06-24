88f542626109fdac171c2599584e09288670709e
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2011-06-24 22:24:08 +0000

    merged with relevant parts of trunk

--- include/spnout.h
+++ include/spnout.h
@@ -1,5 +1,9 @@
+! -- FILE: spnout.h --
       LOGICAL DOSD, DODSO, DOFC, DOSDFC, DOPSO, DOSELE, ANISON,
      &        FCFIN, SPNISO, NCSPNI
-      COMMON /SPNOUT/ ABUND, ISPPRI, DOSD, DODSO, DOFC, DOPSO,
-     &                DOSDFC, DOSELE, ANISON, FCFIN, SPNISO,
-     &                NUCSPI, NCSPNI(MXCENT), ISOTPS(MXCENT)
+      COMMON /SPNOUT/ ABUND,                                        ! real*8
+     &        ISPPRI, ISOTPS(MXCENT),                               ! integer
+     &        NSTATS, NSTATT, NSTATI, NSTATF, NITRST, NUCSPI,
+     &        DOSD, DODSO, DOFC, DOSDFC, DOPSO, DOSELE, ANISON,     ! logical
+     &        FCFIN, SPNISO, NCSPNI(MXCENT)
+! -- end of spnout.h --
