8fa491933781de8538adbf2c5822e452beed37fc
Sarai Dery Folkestad <sarai@dhcp-10-24-5-217.wlan.ntnu.no>
2017-01-26 11:07:34 +0100

    mlcc_mod_utilities module

---
+++ DALTON/mlcc_mod/mlcc_mod_utilities.F90
@@ -0,0 +1,14 @@
+module mlcc_mod_utilities
+function index_t2(nai,nbj)
+!   
+   implicit none
+!
+   integer, intent(in)     :: nai, nbj
+   integer, intent(out)    :: index_t2
+
+   index_t2 = max(nai,nbj)*(max(nai,nbj)-3)/2+nai+nbj
+
+end function
+
+
+end module mlcc_mod_utilities
