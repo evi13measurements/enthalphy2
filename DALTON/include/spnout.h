3e2804bb58ef9eb179434b04bfdea1c593b79c7c
patprovasi <patprovasi@gmail.com>
2017-03-03 15:17:38 -0300

    modified:   DALTON/abacus/abadrv.F 	modified:   DALTON/abacus/abaloc.F 	modified:   DALTON/abacus/abamag.F 	modified:   DALTON/abacus/abasos.F 	modified:   DALTON/include/locinf.h 	modified:   DALTON/include/spnout.h 	modified:   DALTON/rsp/mcdrsp.F 	modified:   DALTON/rsp/rspc82.F 	modified:   DALTON/sirius/sircmo.F

--- DALTON/include/spnout.h
+++ DALTON/include/spnout.h
@@ -1,11 +1,13 @@
 ! -- FILE: spnout.h --
       LOGICAL DOSD, DODSO, DOFC, DOSDFC, DOPSO, DOSELE, ANISON,
      &        FCFIN, SPNISO, NCSPNI,
-     &        SOS, SOSSPN, SOSOCC, SOSOCS
-      COMMON /SPNOUT/ ABUND,                                        ! real*8
-     &        ISPPRI, ISOTPS(MXCENT),                               ! integer
+CPFP Added SOSOSC
+     &        SOS, SOSSPN, SOSOCC, SOSOCS, SOSOSC
+Cend-PFP
+      COMMON /SPNOUT/ ABUND,                                             ! real*8
+     &        ISPPRI, ISOTPS(MXCENT), ISINGUL,                           ! integer
      &        NSTATS, NSTATT, NSTATI, NSTATF, NITRST, NUCSPI,
-     &        DOSD, DODSO, DOFC, DOSDFC, DOPSO, DOSELE, ANISON,     ! logical
+     &        DOSD, DODSO, DOFC, DOSDFC, DOPSO, DOSELE, ANISON,          ! logical
      &        FCFIN, SPNISO, NCSPNI(MXCENT),
-     &        SOS, SOSSPN, SOSOCC, SOSOCS
+     &        SOS, SOSSPN, SOSOCC, SOSOCS, SOSOSC, SINGUL
 ! -- end of spnout.h --
