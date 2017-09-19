ee7a7aae5f343ff22f09845fca3d8db68b8a940e
Hans Jørgen Aagaard Jensen <hjj@sdu.dk>
2017-09-19 19:25:24 +0200

    hjaaj: adding WESTA/ directory with WESTA program files version Sep 2011
    - WESTA is a program from late 1980s using MCSCF interface files from Dalton
          (interface files written if .WESTA specified in Dalton input).
    - WESTA was written for non-adiabatic coupling, diagonal adiabatic correction,
          X-ray spectra.

---
+++ WESTA/westa/westap.h
@@ -0,0 +1,7 @@
+C
+C     /WESTAP/ has files for
+C              geometries A and B, state 1 and 2
+C
+      PARAMETER (MSTAT2 = 40)
+      COMMON /WESTAP/ LUIT1V(2,2),LUSYMV(2),LUPROPV(2),
+     *                LUSPCW,LUSPEC(MSTAT2),LUTMPW
